d852888a1f62274977765fad50853888e3342939
MarcelFeo <marcelfeo29@gmail.com>
2022-04-12 11:44:03 -0300

    finalizando o layout da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Main, Section, Sticky } from "./styles";
+
+import GlobalStyles from "./styles/global";
 
 function App() {
   return (
-    <h1>ok.</h1>
+    <>
+      <Main style={{ height: "1610vh" }}>
+        <Section style={{ height: "72.7%" }}>
+          <Sticky className="first" />
+          <Sticky className="second" />
+        </Section>
+        
+        <Section style={{ height: "9.7%" }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: "10.1%" }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+
+      <GlobalStyles />
+    </>
   );
 }
 
