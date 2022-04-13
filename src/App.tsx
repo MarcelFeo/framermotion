3a6a9daa02d99753502338c9c73037e109cda796
MarcelFeo <marcelfeo29@gmail.com>
2022-04-13 11:16:19 -0300

    finalizando as animações

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
-import React from "react";
-import { Main, Section, Sticky } from "./styles";
+import React from 'react'
 
-import GlobalStyles from "./styles/global";
+import { Main, Section, Sticky } from './styles'
+
+import GlobalStyles from './styles/global'
+import Header from './components/Header'
+import FirstAndSecond from './components/FirstAndSecond'
+import Footer from './components/Footer'
 
 function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
+
   return (
     <>
-      <Main style={{ height: "1610vh" }}>
-        <Section style={{ height: "72.7%" }}>
-          <Sticky className="first" />
-          <Sticky className="second" />
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        <Header />
+
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
         </Section>
-        
-        <Section style={{ height: "9.7%" }}>
+
+        <Section style={{ height: '9.7%' }}>
           <Sticky className="third" />
         </Section>
 
-        <Section style={{ height: "10.1%" }}>
+        <Section style={{ height: '10.1%' }}>
           <Sticky className="fourth" />
         </Section>
-      </Main>
 
-      <GlobalStyles />
+        <Footer />
+      </Main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,45 +1,64 @@
-import { motion } from "framer-motion";
-import styled from "styled-components";
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 export const Main = styled.main`
-    .first {
-        background: red;
-        overflow: hidden;
-
-        .offset {}
-
-        .a {
-            background: #f5f1ea;
+  .first {
+    background: red;
+    overflow: hidden;
+    .offset {
+      width: 100%;
+      height: 100%;
+    }
+    .a {
+      background: #f5f1ea;
+      height: 100%;
+      display: flex;
+      align-items: flex-end;
+      .left-side {
+        width: 50%;
+        height: 100%;
+        background: #7dbb91;
+      }
+      .right-side {
+        width: 50%;
+        height: 100%;
+        .right-image {
+          background: #7dbb91;
+          width: 100%;
+          height: 100%;
         }
-        .b {
-            background: #582534;
-        }
-        .c {
-            background: #0a7397;
-        }
+      }
     }
-
-    .second {
-        background: green; 
+    .b {
+      width: 100%;
+      height: 100%;
+      background: #582534;
     }
-
-    .third {
-        background: yellow; 
+    .c {
+      width: 100%;
+      height: 100%;
+      background: #0a7397;
     }
-
-    .fourth {
-        background: blue;
-    }
-`;
+  }
+  .second {
+    background: green;
+  }
+  .third {
+    background: yellow;
+  }
+  .fourth {
+    background: blue;
+  }
+`
 
 export const Section = styled.div`
-    position: relative;
-`;
+  position: relative;
+`
 
-export const Sticky = styled.div`
-    height: 100vh;
-    position: sticky;
-    top: 0;
-    left: 0;
-    right: 0;
-`;
+export const Sticky = styled(motion.div)`
+  height: 100vh;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+`
